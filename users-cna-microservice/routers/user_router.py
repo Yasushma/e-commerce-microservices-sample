4efaabefaa77215fe11c87d9df1c7517a72d1152
KOTA YASUSHMA <94449243+Yasushma@users.noreply.github.com>
2023-11-10 10:21:29 +0530

    Update user_router.py

--- users-cna-microservice/routers/user_router.py
+++ users-cna-microservice/routers/user_router.py
@@ -24,6 +24,6 @@ async def update_user(user_id: int, name: Optional[str] = None, email: Optional[
 async def get_user(user_id: int, user_dal: UserDAL = Depends(get_user_dal)):
     return await user_dal.get_user(user_id)
 
-@router.get("/users")
+@router.get("/users", response_model=List[UserResponse])
 async def get_all_users(user_dal: UserDAL = Depends(get_user_dal)) -> List[User]:
     return await user_dal.get_all_users()
